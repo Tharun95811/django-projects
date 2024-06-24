cc8267fbef73826e246c08dd1f60dcb113830ef3
Tharun k <116543911+Tharun95811@users.noreply.github.com>
2024-06-24 22:47:41 +0530

    Add files via upload

---
+++ app/studentcourse/forms.py
@@ -0,0 +1,8 @@
+from studentcourse.models import students
+from django import forms
+
+
+class inputform(forms.ModelForm):
+    class Meta:
+        model=students
+        fields=['name1','college1','course1']
